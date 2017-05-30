3a51704b98c643460b736dcaf4d3904fa9829dff
shandongfx <shandongfx@gmail.com>
2017-05-30 13:53:46 -0500

    fix encoding to UTF-8

--- global.R
+++ global.R
@@ -1,5 +1,5 @@
 languages_raw <- read.csv("raw_data_clean/web translation_v1.csv", header = TRUE, as.is = TRUE) 
 languages <- as.data.frame(t(languages_raw[,c("en","sp")]))
 colnames(languages) <- languages_raw[,"key"]
-LL <- 2
+LL <- 1
 #input$runrunrun
